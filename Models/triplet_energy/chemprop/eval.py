a56d40b9508bc77bb9c9d6c5142dd46ebbcb2c21
Leon Schlosser <92179005+le-schlo@users.noreply.github.com>
2024-01-17 13:09:05 +0100

    Update eval.py

--- Models/triplet_energy/chemprop/eval.py
+++ Models/triplet_energy/chemprop/eval.py
@@ -5,6 +5,8 @@ arguments = [
     '--test_path', '/dev/null',
     '--preds_path', '/dev/null',
     '--checkpoint_dir', '/Models/triplet_energy/chemprop/'
+    '--features_generator', 'rdkit_2d_normalized',
+    '--no_features_scaling'
 ]
 
 args = chemprop.args.PredictArgs().parse_args(arguments)
